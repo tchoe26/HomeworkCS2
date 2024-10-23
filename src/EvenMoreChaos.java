16918cde646fda1f81af5a7e5da2e7611191296e
teddy <theodore_choe26@milton.edu>
2024-10-23 16:46:34 -0400

    Initial Commit - 10.24 graded homework

---
+++ src/EvenMoreChaos.java
@@ -0,0 +1,38 @@
+
+//instructions - predict exactly what the print window will look like.
+public class EvenMoreChaos {
+    public int EvenMoreChaos = 14;
+
+    public static void main(String[] args) {
+        EvenMoreChaos EvenMoreChaos = new EvenMoreChaos();
+    }
+    public EvenMoreChaos() {
+
+
+        for (int EvenMoreChaos=0; EvenMoreChaos<10; EvenMoreChaos++) {
+            System.out.println(this.EvenMoreChaos+(EvenMoreChaos%4));
+
+        }
+        int EvenMoreChaos = 2;
+        EvenMoreChaos(this.EvenMoreChaos+(17+EvenMoreChaos));
+
+        if (this.EvenMoreChaos-EvenMoreChaos ==10) {
+            EvenMoreChaos = this.EvenMoreChaos/2;
+            System.out.println(EvenMoreChaos);
+        } else {
+            System.out.println("14" + (this.EvenMoreChaos-EvenMoreChaos));
+            this.EvenMoreChaos = (this.EvenMoreChaos-EvenMoreChaos);
+        }
+        EvenMoreChaos(this.EvenMoreChaos);
+
+    }
+
+    public void EvenMoreChaos(int EvenMoreChaos) {
+        System.out.println(this.EvenMoreChaos+14);
+        this.EvenMoreChaos++;
+        if (EvenMoreChaos%3==0) {
+            EvenMoreChaos(1);
+            this.EvenMoreChaos = EvenMoreChaos;
+        }
+    }
+}
